077f974e6216ae9f092c29ec3fa3d662c984416e
ikhsan-git <ikhsanudinmuhammad12@gmail.com>
2023-04-19 01:00:47 +0700

    clone login page instagram vMobile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import logo from './image/logo.png'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="drop-down">
+     <Dropdown>
+      <Dropdown.Toggle variant="none" id="dropdown-basic">
+        Indonesia (Indonesia)
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Indonesia</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">English</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Arab</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+     </div>
+
+
+     <div className="form">
+     <Form>
+     <div className="logo">
+      <img src={logo} alt="" />
+     </div>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control className='fh' type="email" placeholder="Nomer telepon, email atau nama pengguna" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control className='fh' type="password" placeholder="Kata Sandi" />
+      </Form.Group>
+      
+      <Button className='b-login' variant="primary" type="submit">
+        Masuk
+      </Button>
+    </Form>
+     <p className='help'>Lupa detail login Anda?<strong>Dapatkan bantuan untuk login.</strong></p>
+     </div>
+
+     <h6>ATAU</h6>
+
+     <Button className='b-login after'>
+      Lanjutkan sebagai ......
+     </Button>
+
+    <footer><p>Tidak punya akun?<strong>Buat akun.</strong></p></footer>
     </div>
   );
 }
